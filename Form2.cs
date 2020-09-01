944b3563a01f1e96f70b1ef3ec203b1a93fa26a9
neema sony <neemjac@gmail.com>
2020-09-01 15:49:52 +0530

    first

---
+++ Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LuluSupermarket
+{
+    public partial class form_staff : Form
+    {
+        public form_staff()
+        {
+            InitializeComponent();
+        }
+    }
+}
